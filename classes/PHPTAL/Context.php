b870d5ced2296ad3d5cbed58c074475723ef2b54
kornel <kornel@4d7f0bc2-b5e2-0310-bd1d-fbff8d7f0049>
2010-11-16 16:44:59 +0000

    Restored previous phptal_isempty() behavior, moved new behavior to phptal_true()
    git-svn-id: https://svn.motion-twin.com/phptal/trunk@1010 4d7f0bc2-b5e2-0310-bd1d-fbff8d7f0049

--- classes/PHPTAL/Context.php
+++ classes/PHPTAL/Context.php
@@ -482,7 +482,8 @@ function phptal_path($base, $path, $nothrow=false)
  */
 function phptal_isempty($var)
 {
-    return !$var || ($var instanceof Countable && count($var)===0);
+    return $var === null || $var === false || $var === ''
+           || ((is_array($var) || $var instanceof Countable) && count($var)===0);
 }
 
 /**
